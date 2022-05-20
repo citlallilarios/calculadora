66a7f7364751e4c0e81f4a7955c989279e6fe9df
Citlalli Larios Huerta <isc18350321@gmail.com>
2022-05-20 13:52:55 -0500

    terminacion de calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,242 @@
+import "./App.css";
+import React, { useState } from "react";
+
 function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [currentOperation, setCurrentOperation] = useState("");
+  const [result, setResult] = useState(0);
+
+  let initialState = JSON.parse(localStorage.getItem("historial")) || [];
+  const [historial, sethistorial] = useState(initialState);
+
+  const borrarHistorial =()=>{
+    sethistorial([])
+    localStorage.setItem("historial", JSON.stringify([]));
+  }
+
+  function allClear() {
+    setNumber1("");
+    setNumber2("");
+    setCurrentOperation("");
+    setResult(0);
+  }
+
+  const deleteNumber = () => {
+    if (currentOperation === "") {
+      setNumber1(number1.toString().slice(0, -1));
+    } else {
+      setNumber2(number2.toString().slice(0, -1));
+    }
+  };
+
+  function clickNumber(val) {
+    if (currentOperation === "") {
+      setNumber1(number1 + val);
+    } else {
+      setNumber2(number2 + val);
+    }
+  }
+
+  function clickOperation(val) {
+    setCurrentOperation(val);
+  }
+
+  function getResult(val) {
+    let resultado = 0;
+    switch (currentOperation) {
+      case "+":
+        resultado = Number(number1) + Number(number2);
+        break;
+      case "-":
+        resultado = Number(number1) - Number(number2);
+        break;
+      case "*":
+        resultado = Number(number1) * Number(number2);
+        break;
+      case "/":
+        resultado = Number(number1) / Number(number2);
+        break;
+      default:
+        break;
+    }
+
+    setResult(resultado);
+    const coleccion = {
+      n1: number1,
+      ope: currentOperation,
+      n2: number2,
+      res: resultado,
+    };
+    console.log({ resultado });
+    console.log({ coleccion });
+    const nuevoArray = [...historial, coleccion];
+    sethistorial([...nuevoArray]);
+    localStorage.setItem("historial", JSON.stringify(nuevoArray));
+  }
+
   return (
     <div className="App">
-   <h1>Hello word I'm bitch
-     
-   </h1>
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">
+            {currentOperation ? number1 + currentOperation : ""}
+          </div>
+          <div className="current-operand">
+            {result ? result : !currentOperation ? number1 : number2}
+          </div>
+        </div>
+        <button onClick={allClear} className="span-two">
+          AC
+        </button>
+
+        <button onClick={deleteNumber}>DEL</button>
+        <button
+          onClick={() => {
+            clickOperation("/");
+          }}
+          className="com"
+        >
+          /
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(7);
+          }}
+        >
+          7
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(8);
+          }}
+        >
+          8
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(9);
+          }}
+        >
+          9
+        </button>
+        <button
+          onClick={() => {
+            clickOperation("*");
+          }}
+          className="com"
+        >
+          *
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(4);
+          }}
+        >
+          4
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(5);
+          }}
+        >
+          5
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(6);
+          }}
+        >
+          6
+        </button>
+        <button
+          onClick={() => {
+            clickOperation("+");
+          }}
+          className="com"
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(1);
+          }}
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(2);
+          }}
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(3);
+          }}
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            clickOperation("-");
+          }}
+          className="com"
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(".");
+          }}
+        >
+          .
+        </button>
+        <button
+          onClick={() => {
+            clickNumber(0);
+          }}
+        >
+          0
+        </button>
+        <button onClick={getResult} className="span-two">
+          =
+        </button>
+      </div>
+      {/* BOton para guardar */}
+      <span className="col">
+        <h3>Historial</h3>
+        <br />
+        <br />
+        <br />
+        {historial.length === 0 && "empieza tu Historial"}
+        {historial.length !== 0 && (
+          <ol>
+            <br />
+            <br />
+            <br />
+            {historial.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item.n1}
+                  {item.ope}
+                  {item.n2}={item.res}
+                </li>
+              
+              );
+            })}
+              <button
+              type="button"
+              className="btn btn-primary"
+              onClick={borrarHistorial}
+              disabled={historial.length===0}
+              >
+                Limpiar historial
+              </button>
+          </ol>
+        )}
+      </span>
     </div>
   );
 }
